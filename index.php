abec9d46a69749b9fc3e58cd27590a73452e1617
Fisny <bardi.fisniku@gmail.com>
2021-08-27 15:01:43 +0200

    start

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container-fluid">
+        <!-- top bar -->
+        <div class="row">
+
+            <div class="col d-flex justify-content-between">
+                <div>
+                    <a href="https://policies.google.com/"><img class="google-img" src="https://loghi-famosi.com/wp-content/uploads/2020/09/Google-Logo.png" alt=""></a>
+                    <a class="termini" href="https://policies.google.com/" alt="Privacy e termini">Privacy e termini</a>
+                </div>
+
+                <div>
+                    <a href="https://policies.google.com/"><i class="fas fa-bars"></i></a>
+                    <a class="termini" href="https://policies.google.com/" alt="Privacy e termini"><i class="fas fa-user"></i></a>
+                </div>
+
+            </div>
+
+        </div>
+       
+    </div>
+    <?php
+
+    ?>
+
+</body>
+</html>
