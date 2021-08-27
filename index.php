36cc6aa98abf34e4c3645141e8398a9b9cab6925
Fisny <bardi.fisniku@gmail.com>
2021-08-27 15:22:08 +0200

    hover

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>faq</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -15,7 +20,7 @@
         <!-- top bar -->
         <div class="row">
 
-            <div class="col d-flex justify-content-between">
+            <div class="col d-flex justify-content-between align-items-center">
                 <div>
                     <a href="https://policies.google.com/"><img class="google-img" src="https://loghi-famosi.com/wp-content/uploads/2020/09/Google-Logo.png" alt=""></a>
                     <a class="termini" href="https://policies.google.com/" alt="Privacy e termini">Privacy e termini</a>
@@ -28,6 +33,17 @@
 
             </div>
 
+        </div>
+
+        <div class="row">
+            <div class="col barra">
+                <a href="https://policies.google.com/">Introduzioni</a>
+                <a href="https://policies.google.com/privacy">Norme sulla privacy</a>
+                <a href="https://policies.google.com/terms">Termini di servizio</a>
+                <a href="https://policies.google.com/technologies">Tecnologie</a>
+                <a href="https://policies.google.com/faq">Domande Frequenti</a>
+                <hr>
+            </div>
         </div>
        
     </div>
